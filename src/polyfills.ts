a60549859263aeea30b604402ab7fe7f43cd52ef
matias <maty_colazo@hotmail.com>
2025-02-13 17:52:37 -0300

    polyfills.ts

---
+++ src/polyfills.ts
@@ -0,0 +1,8 @@
+import 'zone.js'; // Esto ya está en tu config
+
+// Agrega estos manualmente:
+import 'core-js/es/array';
+import 'core-js/es/object';
+import 'core-js/es/promise';
+import 'core-js/es/symbol';
+import 'core-js/es/string';
